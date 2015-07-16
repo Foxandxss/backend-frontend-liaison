6c165108109de2fff89820f1c057cd8cee92ed15
Jesus Rodriguez <foxandxss@gmail.com>
2015-07-16 23:06:45 +0200

    Nodemon and html5Mode support

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 var koa = require('koa');
 var mount = require('koa-mount');
-var koaStatic = require('koa-static');
-var router = require('koa-router')();
+var views = require('koa-views');
 
 var api = require('./backend/app');
 
 var app = koa();
-app.use(koaStatic(__dirname + '/frontend/public'));
+
+app.use(views(__dirname + '/frontend/public'));
 
 app.use(mount(api));
+
+app.use(function *() {
+  // For html5Mode
+  var foo = yield this.render('index');
+});
 
 app.listen(3000, function() {
   console.log('Dev server running at 3000');
--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,9 +1,26 @@
 import angular from 'angular';
+import uirouter from 'angular-ui-router';
 
-angular.module('app', []).controller('MainCtrl', function($http) {
+angular.module('app', [uirouter])
+  .config(configUiRouter)
+  .controller('MainCtrl', MainCtrl);
+
+function configUiRouter($urlRouterProvider, $locationProvider, $stateProvider) {
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise('/home');
+
+  $stateProvider.state('home', {
+    url: '/home',
+    template: 'Hello, data from server: {{vm.data}}',
+    controller: 'MainCtrl',
+    controllerAs: 'vm'
+  });
+}
+
+function MainCtrl($http) {
   var vm = this;
 
   $http.get('/api/foo').then(function(result) {
     vm.data = result.data;
   })
-});
+}
